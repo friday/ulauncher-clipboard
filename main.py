69fa7d3e37be5543d26ed1eac932d3e139d3e4ed
Albin <mail@albinlarsson.com>
2019-01-13 18:16:38 +0100

    Add missing period to message

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def setManager(name, extension):
         Notify.init("ulauncher-clipboard-extension")
         message = Notify.Notification.new(
             "Ulauncher-clipboard error",
-            "Could not load {}. Make sure it's installed and enabled".format(manager.name),
+            "Could not load {}. Make sure it's installed and enabled.".format(manager.name),
             icon.get_filename()
         )
 
